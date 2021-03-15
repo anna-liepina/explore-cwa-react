fad4dcd160b32c4f5d8c2963220e3c553786ebcb
Anna Liepina <anna.liepina@yahoo.co.uk>
2021-03-15 18:42:32 +0000

    disable test

--- src/component/drawer.test.jsx
+++ src/component/drawer.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Drawer from './drawer';
 
-describe('<Drawer/>', () => {
+describes.skip('<Drawer/>', () => {
     const props = {
         onClose: jest.fn(),
     };
