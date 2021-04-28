3a80e81c1e9e3253f1b65b66dd6b2393da231968
Anna Liepina <anna.liepina@yahoo.co.uk>
2021-04-28 14:52:10 +0100

    codestyle

--- cypress/specs/layout/tab-handler.design-match.spec.js
+++ cypress/specs/layout/tab-handler.design-match.spec.js
@@ -1,7 +1,4 @@
-describe('DESIGN MATCH - User Navigation',   {
-    viewportHeight: 1000,
-    viewportWidth: 1600,
-  }, () => {
+describe('DESIGN MATCH - User Navigation', () => {
     it('as I user, I expect tabs to be responsive', () => {
         cy
             .fixture('screen_breakpoints')
@@ -9,8 +6,7 @@ describe('DESIGN MATCH - User Navigation',   {
                 cy.visit('/');
 
                 for (const [label, width] of matrix) {
-                    cy
-                        .viewport(parseInt(width, 10), 1000)
+                    cy.viewport(width, 1000);
                     cy
                         .get('.tab-handler')
                         .matchImageSnapshot(`tab-handler--${width}px-${label}`);
