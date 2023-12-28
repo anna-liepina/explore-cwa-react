d79cc4bf2f11d1c4cc13d9a1934d4c81d0632e2f
Anna Liepina <anna.liepina@yahoo.co.uk>
2023-12-28 12:27:08 +0000

    useQuery hook

---
+++ src/hooks/useQuery.test.tsx
@@ -0,0 +1,73 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, act, waitFor } from "@testing-library/react";
+import { useQuery } from "./useQuery";
+
+describe("<useQuery /> hook", () => {
+    const mockFetch = jest.fn();
+
+    const TestComponent = () => {
+        const [state, fetch] = useQuery(mockFetch);
+
+        return <div>
+            <div data-testid="loading">{state.isLoading ? "Loading..." : "false"}</div>
+            <div data-testid="data">{state.data}</div>
+            <div data-testid="errors">{state.errors && state.errors.message}</div>
+            <button onClick={fetch}>Fetch</button>
+        </div>;
+    };
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("internal state ::isLoading need to be true if ::fetch is NOT resolved yet", async () => {
+        render(<TestComponent />);
+
+        await waitFor(() => {
+            expect(screen.getByTestId("loading")).toHaveTextContent("Loading...");
+        });
+    });
+
+    it("internal state ::isLoading need to be false if ::fetch is resolved successfully", async () => {
+        mockFetch.mockResolvedValue("testData");
+
+        await act(async () => {
+            render(<TestComponent />);
+        });
+
+        expect(screen.getByTestId("loading")).toHaveTextContent("false");
+        expect(screen.getByTestId("data")).toHaveTextContent("testData");
+        expect(screen.getByTestId("errors")).toHaveTextContent("");
+    });
+
+    it("internal state ::isLoading need to be false if ::fetch is resolved with error", async () => {
+        const errorMessage = "Fetch error";
+        mockFetch.mockRejectedValue(new Error(errorMessage));
+
+        await act(async () => {
+            render(<TestComponent />);
+        });
+
+        expect(screen.getByTestId("loading")).toHaveTextContent("false");
+        expect(screen.getByTestId("data")).toHaveTextContent("");
+        expect(screen.getByTestId("errors")).toHaveTextContent(errorMessage);
+    });
+
+    it("manual fetching mode", async () => {
+        mockFetch.mockResolvedValue("testData");
+
+        await act(async () => {
+            render(<TestComponent />);
+        });
+
+        expect(screen.getByTestId("loading")).toHaveTextContent("false");
+
+        await act(async () => {
+            screen.getByText("Fetch").click();
+        });
+
+        expect(screen.getByTestId("loading")).toHaveTextContent("false");
+        expect(screen.getByTestId("data")).toHaveTextContent("testData");
+        expect(screen.getByTestId("errors")).toHaveTextContent("");
+    });
+});
---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect, useCallback } from 'react';
+
+export interface IQueryState<TData = any, TErrors = any> {
+  isLoading: boolean;
+  data?: TData;
+  errors?: TErrors;
+}
+
+export interface IQueryOptions {
+  manual?: boolean;
+}
+
+export const useQuery = <TArgs = any, TData = any, TErrors = any>(
+    fetch: (args: TArgs) => Promise<TData>,
+    options?: IQueryOptions
+): [IQueryState<TData, TErrors>, (args?: TArgs) => void] => {
+    const [state, setState] = useState<IQueryState<TData, TErrors>>({
+        isLoading: true,
+    });
+
+    const handleFetch = useCallback(
+        async (args?: TArgs) => {
+            try {
+                setState({ isLoading: true });
+                const data = await fetch(args || {} as TArgs); // Use provided args or default to an empty object
+                setState({ data, isLoading: false });
+            } catch (error) {
+                setState({ errors: error as TErrors, isLoading: false });
+            }
+        },
+        [fetch]
+    );
+
+    useEffect(() => {
+        if (!options?.manual) {
+            handleFetch();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return [state, handleFetch];
+};
