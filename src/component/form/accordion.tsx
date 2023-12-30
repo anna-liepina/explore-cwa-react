e76a8237b71eec4c5ba3829cd549ee710ff0350b
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2023-12-30 00:45:32 +0000

    convert <Accordion /> to typescript (#112)
    * repo rework
    
    * fixes
    
    * use IQAProps to inherit data-cy prop
    
    * convert <Suggestions /> to typescript
    
    * fixes
    
    * convert <Accordion /> to typescript
    
    * fixes
    
    * fixes
    
    * Update accordion.tsx
    
    ---------
    
    Co-authored-by: Eugene Matvejev <eugene.matvejev@gmail.com>

---
+++ src/component/form/accordion.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+import type { IQAProps } from '../../utils/commonTypes';
+
+export interface IAccordionProps extends IQAProps {
+    className?: string;
+    title?: string;
+    isCollapsed?: boolean;
+    onCollapse?: () => void;
+}
+
+const Accordion: React.FC<IAccordionProps & PropsWithChildren> = ({
+    className,
+    title,
+    onCollapse,
+    isCollapsed,
+    children,
+    ...props
+}) => <section className={classNames(className, { accordion: !isCollapsed, 'accordion--collapsed': isCollapsed })}>
+    {
+        title && 
+        <h2 className="accordion_title" onClick={onCollapse} {...props}>
+            {title}
+        </h2>
+    }
+    {!isCollapsed && children}
+</section>;
+
+export default Accordion;
+
