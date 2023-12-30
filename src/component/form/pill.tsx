28a8dadf1a04102acef21ed0eef8c4ae8e2ca6d0
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2023-12-30 10:22:45 +0000

    Update pill.tsx

--- src/component/form/pill.tsx
+++ src/component/form/pill.tsx
@@ -7,10 +7,11 @@ export interface IPillProps extends IQAProps {
     'data-id'?: string | number;
 }
 
-const Pill: React.FC<IPillProps> = ({ 'data-cy': cy = '', label, 'data-id': id }) => 
+const Pill: React.FC<IPillProps> = ({ 'data-cy': cy = '', label, 'data-id': id }) => (
     <span className="pill" data-cy={cy}>
         {label}
         {undefined !== id && <button data-cy={`${cy}-remove`} data-id={id} className="pill__control">×</button>}
-    </span>;
+    </span>
+);
 
 export default Pill;
