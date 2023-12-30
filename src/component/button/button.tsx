f691a1dfbe10e60771ed3bf704a2082ac6eefd5d
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2023-12-30 07:59:16 +0000

    convert <Button /> to typescript (#114)
    * repo rework
    
    * fixes
    
    * use IQAProps to inherit data-cy prop
    
    * convert <Suggestions /> to typescript
    
    * fixes
    
    * convert <Accordion /> to typescript
    
    * convert <HTMLInput /> to typescript
    
    * convert <Button /> to typescript
    
    * Update button.tsx
    
    * Update accordion.tsx

--- src/component/button/button.tsx
+++ src/component/button/button.tsx
@@ -1,11 +1,13 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React from 'react';
+import type { ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames';
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     label: string;
 }
 
-const Button: React.FC<IButtonProps> = ({ className = '', label, ...props }) => (
-    <button className={`button ${className}`} {...props}>
+const Button: React.FC<IButtonProps> = ({ className, label, ...props }) => (
+    <button className={classNames(`button`, className)} {...props}>
         {label}
     </button>
 );
