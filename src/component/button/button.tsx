29e88d5d3b40ab778137504457b4e3cec159a466
Anna Liepina <anna.liepina@yahoo.co.uk>
2023-12-30 00:10:18 +0000

    convert <Button /> to typescript

--- src/component/button/button.tsx
+++ src/component/button/button.tsx
@@ -1,11 +1,12 @@
+import classNames from 'classnames';
 import React, { ButtonHTMLAttributes } from 'react';
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     label: string;
 }
 
-const Button: React.FC<IButtonProps> = ({ className = '', label, ...props }) => (
-    <button className={`button ${className}`} {...props}>
+const Button: React.FC<IButtonProps> = ({ className, label, ...props }) => (
+    <button className={classNames(`button`, className)} {...props}>
         {label}
     </button>
 );
