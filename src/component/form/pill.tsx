defb36574dc52488f87e28debdae4f310dad0048
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2023-12-30 11:14:25 +0000

    convert <Pill /> to typescript (#116)
    * repo rework
    
    * fixes
    
    * use IQAProps to inherit data-cy prop
    
    * convert <Suggestions /> to typescript
    
    * fixes
    
    * convert <Accordion /> to typescript
    
    * convert <HTMLInput /> to typescript
    
    * convert <Button /> to typescript
    
    * convert <FormField /> to typescript
    
    * convert <Pill /> to typescript
    
    * Update pill.tsx
    
    * Update pill.tsx
    
    * fixes

---
+++ src/component/form/pill.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classnames from 'classnames';
+import type { IQAProps } from '../../utils/commonTypes';
+
+export interface IPillProps extends IQAProps {
+    label: string;
+    className?: string;
+    'data-id'?: string | number;
+}
+
+const Pill: React.FC<IPillProps> = ({ 'data-cy': cy = '', label, className, 'data-id': id }) => (
+    <span className={classnames("pill", className)} data-cy={cy}>
+        {label}
+        {undefined !== id && <button data-cy={`${cy}-remove`} data-id={id} className="pill__control">×</button>}
+    </span>
+);
+
+export default Pill;
