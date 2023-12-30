251b47a834adce54fa5d78c6558b36b291152fc5
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2023-12-30 15:10:26 +0000

    convert <InputWithPills /> to typescript (#117)
    * repo rework
    
    * fixes
    
    * use IQAProps to inherit data-cy prop
    
    * convert <Suggestions /> to typescript
    
    * fixes
    
    * convert <Accordion /> to typescript
    
    * convert <HTMLInput /> to typescript
    
    * convert <Button /> to typescript
    
    * convert <FormField /> to typescript
    
    * convert <Pill /> to typescript
    
    * Update pill.tsx
    
    * Update pill.tsx
    
    * fixes
    
    * convert <InputWithPills /> to typescript
    
    * fixes

---
+++ src/component/form/input-with-pills.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { MouseEventHandler } from 'react';
+import classNames from 'classnames';
+import { PureHTMLInput } from './html-input';
+import Pill from './pill';
+import type { IQAProps } from '../../utils/commonTypes';
+
+export interface IInputWithPillsValue {
+    label: string;
+    value: string | number;
+}
+
+export interface IInputWithPillsProps extends IQAProps {
+    className?: string;
+    value?: IInputWithPillsValue[];
+    onClick?: MouseEventHandler;
+}
+
+const InputWithPills: React.FC<IInputWithPillsProps> = ({
+    'data-cy': cy = '',
+    className,
+    onClick,
+    value,
+    ...props
+}) => (
+    <div className={classNames("input-with-pills", className)} onClick={onClick}>
+        {
+            Array.isArray(value)
+            && value.map((props, i) => <Pill key={i} data-cy={`${cy}-pill-${i}`} data-id={i} {...props} />)
+        }
+        <PureHTMLInput {...props} data-cy={cy} className="input-with-pills__input" />
+    </div>
+);
+
+export default InputWithPills;
