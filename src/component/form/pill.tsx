d062d00d5f2e5be13e8536e0681e33b21fa63a96
Anna Liepina <anna.liepina@yahoo.co.uk>
2023-12-30 10:20:10 +0000

    convert <Pill /> to typescript

---
+++ src/component/form/pill.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import type { IQAProps } from '../../utils/commonTypes';
+
+export interface IPillProps extends IQAProps {
+    label: string;
+    className?: string;
+    'data-id'?: string | number;
+}
+
+const Pill: React.FC<IPillProps> = ({ 'data-cy': cy = '', label, 'data-id': id }) => 
+    <span className="pill" data-cy={cy}>
+        {label}
+        {undefined !== id && <button data-cy={`${cy}-remove`} data-id={id} className="pill__control">×</button>}
+    </span>;
+
+export default Pill;
