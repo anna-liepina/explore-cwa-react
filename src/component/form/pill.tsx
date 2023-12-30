3863aa6f333f02e7a7f687e9a40426cd097f5af2
Anna Liepina <anna.liepina@yahoo.co.uk>
2023-12-30 10:26:41 +0000

    fixes

--- src/component/form/pill.tsx
+++ src/component/form/pill.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+import classnames from 'classnames';
 import type { IQAProps } from '../../utils/commonTypes';
 
 export interface IPillProps extends IQAProps {
