07a4d1dc39320589c9d274be351bd339e6e7037b
Anna Liepina <anna.liepina@yahoo.co.uk>
2024-01-01 21:19:13 +0000

    fix React 18 mounting warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import TopNav from './component/topnav/topnav';
 import routes from './__config/routes';
 import './index.scss';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+root.render(
     <BrowserRouter>
         <TopNav />
         <Switch>
@@ -13,6 +14,5 @@ ReactDOM.render(
                 routes.map((props, i) => <Route key={i} {...props} />)
             }
         </Switch>
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
 );
